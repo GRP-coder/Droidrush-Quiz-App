fdccadff96833ae2d8a8250d46de991bb201f8d0
GRP-coder <grp.psp.07@gmail.com>
2020-10-17 10:57:57 +0530

    fire base integration comit new

--- app/src/main/java/com/example/droidrush_quiz_app/AdminLoginActivity.java
+++ app/src/main/java/com/example/droidrush_quiz_app/AdminLoginActivity.java
@@ -1,14 +1,66 @@
 package com.example.droidrush_quiz_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AdminLoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    EditText emailt, passt;
+    String email, password;
+    Button login;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        login= findViewById(R.id.loginb);
+        emailt= findViewById(R.id.emailet);
+        passt= findViewById(R.id.passet);
+
+        mAuth = FirebaseAuth.getInstance();
+
+            login.setOnClickListener(new View.OnClickListener() {
+               @Override
+                public void onClick(View v) {
+                    email= emailt.getText().toString().trim();
+                    password = passt.getText().toString().trim();
+
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(AdminLoginActivity.this, "Login Succesful", Toast.LENGTH_SHORT).show();
+                                       startActivity(new Intent(getApplicationContext(),AdminControl.class));
+                                    }
+                                    else {
+                                        Toast.makeText(AdminLoginActivity.this, "Authentication failed."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                    }
+
+            });
+        }
     }
-}
+
+
